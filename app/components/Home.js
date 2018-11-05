24117ac3a1193128ad658f3f0fa067fb1ffe5943
tsenglinhsu <vic750221@gmail.com>
2018-11-05 13:46:05 +0800

    WIP

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,6 +9,28 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
+  componentDidMount() {
+    fetch('https://www.ptt.cc/bbs/index.html')
+      .then(response => response.text())
+      .then(str => new DOMParser().parseFromString(str, 'text/html'))
+      .then(data => {
+        const board = data.querySelectorAll('a.board');
+        const boardJSON = {
+          board: Array.from(board).map(e => ({
+            boardLink: e.href,
+            boardName: e.querySelector('div.board-name').textContent,
+            boardNuser: e.querySelector('div.board-nuser').textContent,
+            boardTitle: e.querySelector('div.board-title').textContent
+          }))
+        };
+
+        return console.log(boardJSON);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className={styles.container} data-tid="container">
